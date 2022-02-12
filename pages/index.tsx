83819cf44c879746cfdfa7c4b19920541b4cd60d
Gleb Ryshkov <gleb@ryshkov.com>
2022-02-12 17:44:31 -0500

    fix: fixed home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,8 @@ const Home: NextPage = () => {
               background: "#000",
               color: theme.colors.text_title_light,
               height: theme.spacing(60),
+              marginTop: theme.spacing(9),
+              marginBottom: theme.spacing(9),
             })}
             narrow={true}
           >
@@ -67,186 +69,190 @@ const Home: NextPage = () => {
               Gallery
             </Text>
           </Box>
-        </Box>
-        <Box>
-          <div css={{ display: "flex", columnGap: 30 }}>
-            <div key="1" css={{ width: "50%" }}>
-              <Box
+          <Grid
+            css={(theme) => ({
+              marginBottom: theme.spacing(9),
+              marginTop: theme.spacing(9),
+              alignItems: "flex-start",
+            })}
+          >
+            <Box
+              css={(theme) => ({
+                background: "#510EAC",
+                color: theme.colors.text_title_light,
+                gridColumn: "span 6",
+              })}
+              narrow={true}
+            >
+              <Text variant="h6" css={{ margin: 0, opacity: 0.5 }}>
+                Podcast
+              </Text>
+              <div
                 css={(theme) => ({
-                  background: "#510EAC",
-                  color: theme.colors.text_title_light,
+                  display: "flex",
+                  columnGap: 50,
+                  marginTop: theme.spacing(2),
                 })}
-                narrow={true}
               >
-                <Text variant="h6" css={{ margin: 0, opacity: 0.5 }}>
-                  Podcast
-                </Text>
+                <div css={{ flexGrow: 1 }}>
+                  <Text
+                    component="h3"
+                    css={(theme) => ({
+                      textTransform: "uppercase",
+                      marginTop: 0,
+                      marginBottom: theme.spacing(1),
+                    })}
+                  >
+                    Bram Vanhaeren
+                  </Text>
+                  <Text variant="h6" css={{ margin: 0, opacity: 0.5 }}>
+                    EP01 - 10/13 - 12:00 PST
+                  </Text>
+                  <div
+                    css={(theme) => ({
+                      marginTop: theme.spacing(2),
+                      marginBottom: theme.spacing(2),
+                      display: "flex",
+                      alignItems: "center",
+                      columnGap: theme.spacing(2),
+                    })}
+                  >
+                    <Button Icon={Play} css={{ color: "#510EAC" }}>
+                      Watch
+                    </Button>
+                    <Button
+                      variant="bordered"
+                      size="small"
+                      Icon={Itunes}
+                      css={{ opacity: 0.5 }}
+                    />
+                    <Button
+                      variant="bordered"
+                      size="small"
+                      Icon={Spotify}
+                      css={{ opacity: 0.5 }}
+                    />
+                  </div>
+                  <Text component={Link} href="/" css={{ opacity: 0.5 }}>
+                    All episodes
+                  </Text>
+                </div>
                 <div
                   css={(theme) => ({
-                    display: "flex",
-                    columnGap: 50,
+                    width: theme.spacing(16),
+                    height: theme.spacing(16),
+                    background: "#000",
+                    borderRadius: "50%",
+                    flexShrink: 0,
+                  })}
+                ></div>
+              </div>
+            </Box>
+            <Box
+              css={(theme) => ({
+                background: "#5865F2",
+                color: theme.colors.text_title_light,
+                position: "relative",
+                overflow: "hidden",
+                gridColumn: "7 / span 3",
+              })}
+              narrow={true}
+            >
+              <Discord
+                css={{
+                  "& stop:first-child": {
+                    stopColor: "currentColor",
+                    stopOpacity: 0.5,
+                  },
+                  "& stop:last-child": {
+                    stopColor: "#5865F2",
+                  },
+                  position: "absolute",
+                  right: "0",
+                  top: "50%",
+                  transform: "translate(50%, -50%)",
+                  height: "100%",
+                  width: "100%",
+                  fill: "url(#gradient)",
+                  opacity: 0.5,
+                }}
+              />
+              <div css={{ position: "relative" }}>
+                <Text component="h4" css={{ margin: 0 }}>
+                  Discord
+                </Text>
+                <Text css={{ margin: 0 }}>Join the conversation</Text>
+                <div
+                  css={(theme) => ({
                     marginTop: theme.spacing(2),
+                    marginBottom: theme.spacing(2),
                   })}
                 >
-                  <div css={{ flexGrow: 1 }}>
-                    <Text
-                      component="h3"
-                      css={(theme) => ({
-                        textTransform: "uppercase",
-                        marginTop: 0,
-                        marginBottom: theme.spacing(1),
-                      })}
-                    >
-                      Bram Vanhaeren
-                    </Text>
-                    <Text variant="h6" css={{ margin: 0, opacity: 0.5 }}>
-                      EP01 - 10/13 - 12:00 PST
-                    </Text>
-                    <div
-                      css={(theme) => ({
-                        marginTop: theme.spacing(2),
-                        marginBottom: theme.spacing(2),
-                        display: "flex",
-                        alignItems: "center",
-                        columnGap: theme.spacing(2),
-                      })}
-                    >
-                      <Button Icon={Play} css={{ color: "#510EAC" }}>
-                        Watch
-                      </Button>
-                      <Button
-                        variant="bordered"
-                        size="small"
-                        Icon={Itunes}
-                        css={{ opacity: 0.5 }}
-                      />
-                      <Button
-                        variant="bordered"
-                        size="small"
-                        Icon={Spotify}
-                        css={{ opacity: 0.5 }}
-                      />
-                    </div>
-                    <Text component={Link} href="/" css={{ opacity: 0.5 }}>
-                      All episodes
-                    </Text>
-                  </div>
-                  <div
-                    css={(theme) => ({
-                      width: theme.spacing(16),
-                      height: theme.spacing(16),
-                      background: "#000",
-                      borderRadius: "50%",
-                      flexShrink: 0,
-                    })}
-                  ></div>
+                  <Button Icon={Discord} css={{ color: "#5865F2" }}>
+                    Join
+                  </Button>
                 </div>
-              </Box>
-            </div>
-            <div css={{ width: "25%" }}>
-              <Box
-                css={(theme) => ({
-                  background: "#5865F2",
-                  color: theme.colors.text_title_light,
-                  position: "relative",
-                  overflow: "hidden",
-                })}
-                narrow={true}
-              >
-                <Discord
-                  css={{
-                    "& stop:first-child": {
-                      stopColor: "currentColor",
-                      stopOpacity: 0.5,
-                    },
-                    "& stop:last-child": {
-                      stopColor: "#5865F2",
-                    },
-                    position: "absolute",
-                    right: "0",
-                    top: "50%",
-                    transform: "translate(50%, -50%)",
-                    height: "100%",
-                    width: "100%",
-                    fill: "url(#gradient)",
-                    opacity: 0.5,
-                  }}
-                />
-                <div css={{ position: "relative" }}>
-                  <Text component="h4" css={{ margin: 0 }}>
-                    Discord
-                  </Text>
-                  <Text css={{ margin: 0 }}>Join the conversation</Text>
-                  <div
-                    css={(theme) => ({
-                      marginTop: theme.spacing(2),
-                      marginBottom: theme.spacing(2),
-                    })}
-                  >
-                    <Button Icon={Discord} css={{ color: "#5865F2" }}>
-                      Join
-                    </Button>
-                  </div>
-                  <Text component={Link} href="/" css={{ opacity: 0.5 }}>
-                    2910 Members
-                  </Text>
+                <Text component={Link} href="/" css={{ opacity: 0.5 }}>
+                  2910 Members
+                </Text>
+              </div>
+            </Box>
+            <Box
+              css={(theme) => ({
+                background: "#489BE9",
+                color: theme.colors.text_title_light,
+                position: "relative",
+                overflow: "hidden",
+                gridColumn: "10 / span 3",
+              })}
+              narrow={true}
+            >
+              <Twitter
+                css={{
+                  "& stop:first-child": {
+                    stopColor: "currentColor",
+                    stopOpacity: 0.5,
+                  },
+                  "& stop:last-child": {
+                    stopColor: "#489BE9",
+                  },
+                  position: "absolute",
+                  right: "0",
+                  top: "50%",
+                  transform: "translate(50%, -50%)",
+                  height: "100%",
+                  width: "100%",
+                  fill: "url(#gradient)",
+                  opacity: 0.5,
+                }}
+              />
+              <div css={{ position: "relative" }}>
+                <Text component="h4" css={{ margin: 0 }}>
+                  Twitter
+                </Text>
+                <Text css={{ margin: 0 }}>Follow our latest news</Text>
+                <div
+                  css={(theme) => ({
+                    marginTop: theme.spacing(2),
+                    marginBottom: theme.spacing(2),
+                  })}
+                >
+                  <Button Icon={Twitter} css={{ color: "#489BE9" }}>
+                    Follow
+                  </Button>
                 </div>
-              </Box>
-            </div>
-            <div key="3" css={{ width: "25%" }}>
-              <Box
-                css={(theme) => ({
-                  background: "#489BE9",
-                  color: theme.colors.text_title_light,
-                  position: "relative",
-                  overflow: "hidden",
-                })}
-                narrow={true}
-              >
-                <Twitter
-                  css={{
-                    "& stop:first-child": {
-                      stopColor: "currentColor",
-                      stopOpacity: 0.5,
-                    },
-                    "& stop:last-child": {
-                      stopColor: "#489BE9",
-                    },
-                    position: "absolute",
-                    right: "0",
-                    top: "50%",
-                    transform: "translate(50%, -50%)",
-                    height: "100%",
-                    width: "100%",
-                    fill: "url(#gradient)",
-                    opacity: 0.5,
-                  }}
-                />
-                <div css={{ position: "relative" }}>
-                  <Text component="h4" css={{ margin: 0 }}>
-                    Twitter
-                  </Text>
-                  <Text css={{ margin: 0 }}>Follow our latest news</Text>
-                  <div
-                    css={(theme) => ({
-                      marginTop: theme.spacing(2),
-                      marginBottom: theme.spacing(2),
-                    })}
-                  >
-                    <Button Icon={Twitter} css={{ color: "#489BE9" }}>
-                      Follow
-                    </Button>
-                  </div>
-                  <Text component={Link} href="/" css={{ opacity: 0.5 }}>
-                    9910 Followers
-                  </Text>
-                </div>
-              </Box>
-            </div>
-          </div>
-        </Box>
-        <Box>
-          <Grid>
+                <Text component={Link} href="/" css={{ opacity: 0.5 }}>
+                  9910 Followers
+                </Text>
+              </div>
+            </Box>
+          </Grid>
+          <Grid
+            css={(theme) => ({
+              marginBottom: theme.spacing(10),
+              marginTop: theme.spacing(16),
+            })}
+          >
             <div css={{ gridColumn: "span 3", textAlign: "center" }}>
               <Esquire />
             </div>
@@ -260,13 +266,18 @@ const Home: NextPage = () => {
               <DigitalArts />
             </div>
           </Grid>
-          <Box>
-            <Quote>
+          <Grid
+            css={(theme) => ({
+              marginBottom: theme.spacing(10),
+              marginTop: theme.spacing(10),
+            })}
+          >
+            <Quote css={{ gridColumn: "2 / span 10" }}>
               “Two is a sign of union. And diamonds are a sign of prosperity. I
               wanted to show the duality using my Ugly character wearing a
               twofaced mask.”
             </Quote>
-          </Box>
+          </Grid>
         </Box>
       </Layout>
     </GlobalLayout>
