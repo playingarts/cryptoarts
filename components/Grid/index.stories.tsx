01b496b0abf4e7bea65981fc415234f50691b439
Gleb Ryshkov <gleb@ryshkov.com>
2022-02-13 10:21:27 -0500

    test: added grid cases

--- components/Grid/index.stories.tsx
+++ components/Grid/index.stories.tsx
@@ -7,24 +7,33 @@ export default {
 } as ComponentMeta<typeof Grid>;
 
 const Template: ComponentStory<typeof Grid> = (args) => (
-  <Grid {...args}>
-    <div css={{ gridColumn: "span 6", background: "lightgray" }}>1/2</div>
-    <div css={{ gridColumn: "span 6", background: "lightgray" }}>1/2</div>
+  <Grid {...args} css={(theme) => ({ rowGap: theme.spacing(3) })}>
+    <div css={{ gridColumn: "span 6", background: "lightgray" }}>6 columns</div>
+    <div css={{ gridColumn: "span 6", background: "lightgray" }}>6 columns</div>
 
-    <div css={{ gridColumn: "span 4", background: "lightgray" }}>1/3</div>
-    <div css={{ gridColumn: "span 4", background: "lightgray" }}>1/3</div>
-    <div css={{ gridColumn: "span 4", background: "lightgray" }}>1/3</div>
+    <div css={{ gridColumn: "span 4", background: "lightgray" }}>4 columns</div>
+    <div css={{ gridColumn: "span 4", background: "lightgray" }}>4 columns</div>
+    <div css={{ gridColumn: "span 4", background: "lightgray" }}>4 columns</div>
 
-    <div css={{ gridColumn: "span 3", background: "lightgray" }}>1/4</div>
-    <div css={{ gridColumn: "span 3", background: "lightgray" }}>1/4</div>
-    <div css={{ gridColumn: "span 3", background: "lightgray" }}>1/4</div>
-    <div css={{ gridColumn: "span 3", background: "lightgray" }}>1/4</div>
+    <div css={{ gridColumn: "span 3", background: "lightgray" }}>3 columns</div>
+    <div css={{ gridColumn: "span 3", background: "lightgray" }}>3 columns</div>
+    <div css={{ gridColumn: "span 3", background: "lightgray" }}>3 columns</div>
+    <div css={{ gridColumn: "span 3", background: "lightgray" }}>3 columns</div>
 
-    <div css={{ gridColumn: "span 9", background: "lightgray" }}>3/4</div>
-    <div css={{ gridColumn: "10 / span 3", background: "lightgray" }}>1/4</div>
+    <div css={{ gridColumn: "span 9", background: "lightgray" }}>9 columns</div>
+    <div css={{ gridColumn: "10 / span 3", background: "lightgray" }}>
+      3 columns
+    </div>
 
     <div css={{ gridColumn: "2 / span 10", background: "lightgray" }}>
-      10/12
+      10 columns
+    </div>
+
+    <div css={{ gridColumn: "2 / span 10" }}>
+      <Grid>
+        <div css={{ gridColumn: "span 2", background: "gray" }}>2 columns</div>
+        <div css={{ gridColumn: "span 8", background: "gray" }}>8 columns</div>
+      </Grid>
     </div>
   </Grid>
 );
