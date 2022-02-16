14e34a2818f7b5e731397292c3c763a0913c0d8f
Gleb Ryshkov <gleb@ryshkov.com>
2022-02-16 18:01:40 -0500

    test: fixed storybook next/image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,14 @@ import { theme } from "../pages/_app";
 import { Links } from "../pages/_document";
 import { RouterContext } from "next/dist/shared/lib/router-context";
 import { MockedProvider } from "@apollo/client/testing";
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
