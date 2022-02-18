929e21bfa32af443833e5d43e81ac6665fb3349b
andrejs <andrejsmatvejevs000@gmail.com>
2022-02-18 20:37:46 +0000

    fix: added random cards to Hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,65 +1,52 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useEffect } from "react";
+import { useLoadRandomCards } from "../../hooks/card";
 import Card from "../Card";
 
 type Props = HTMLAttributes<HTMLElement>;
 
-const Hero: FC<Props> = (props) => (
-  <div
-    {...props}
-    css={{
-      position: "relative",
-    }}
-  >
-    <Card
-      interactive={true}
-      noInfo={true}
-      card={{
-        _id: "61db1bebb3f83f2efa2ea08d",
-        artist: {
-          _id: "_id",
-          name: "name",
-          slug: "slug",
-          userpic: "userpic",
-          social: {},
-        },
-        deck: { _id: "_id", title: "title", info: "info", slug: "slug" },
-        img:
-          "https://s3.amazonaws.com/img.playingarts.com/zero-big-hd/jack-of-hearts-jonathan-wong.jpg",
-        video:
-          "https://s3.amazonaws.com/img.playingarts.com/zero-video/j-hearts_01.mp4",
+const Hero: FC<Props> = (props) => {
+  const { cards, loadRandomCards } = useLoadRandomCards();
+
+  useEffect(() => {
+    loadRandomCards({ variables: { limit: 2, shuffle: true } });
+  }, [loadRandomCards]);
+
+  if (!cards) {
+    return null;
+  }
+
+  return (
+    <div
+      {...props}
+      css={{
+        position: "relative",
       }}
-      css={(theme) => ({
-        transform: "rotate(15deg) scale(0.95)",
-        filter: "blur(1px)",
-        position: "absolute",
-        left: theme.spacing(30),
-      })}
-    />
-    <Card
-      interactive={true}
-      card={{
-        _id: "61db141bc683592b14bb0b8e",
-        video: "",
-        img:
-          "https://s3.amazonaws.com/img.playingarts.com/future/cards/card-gian-wong.jpg",
-        artist: {
-          _id: "_id",
-          name: "name",
-          slug: "slug",
-          userpic: "userpic",
-          social: {},
-        },
-        deck: { _id: "_id", title: "title", info: "info", slug: "slug" },
-      }}
-      size="big"
-      noInfo={true}
-      css={(theme) => ({
-        transform: "rotate(-15deg) scale(0.85)",
-        marginTop: -theme.spacing(8),
-        marginLeft: -theme.spacing(3),
-      })}
-    />
-  </div>
-);
+    >
+      {cards.map((card, index) => (
+        <Card
+          key={card._id}
+          interactive={true}
+          noInfo={true}
+          card={card}
+          size={index % 2 === 0 ? undefined : "big"}
+          css={(theme) =>
+            index % 2 === 0
+              ? {
+                  transform: "rotate(15deg) scale(0.95)",
+                  filter: "blur(1px)",
+                  position: "absolute",
+                  left: theme.spacing(30),
+                }
+              : {
+                  transform: "rotate(-15deg) scale(0.85)",
+                  marginTop: -theme.spacing(8),
+                  marginLeft: -theme.spacing(3),
+                }
+          }
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Hero;
