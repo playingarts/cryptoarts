1cdbb989ddda5a722ab3e577fd46edeb5bcf0bb7
vlad <vlad@korzinin.com>
2025-12-26 20:31:11 +0200

    fix: use any type for express-graphql compatibility

--- pages/api/v1/graphql.ts
+++ pages/api/v1/graphql.ts
@@ -1,5 +1,5 @@
 import { graphqlHTTP } from "express-graphql";
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { schema } from "../../../source/graphql/schema";
 import { connect } from "../../../source/mongoose";
 
@@ -9,10 +9,9 @@ export const config = {
   },
 };
 
-const handler = graphqlHTTP((req) => ({
+const handler = graphqlHTTP(() => ({
   schema,
   graphiql: process.env.NODE_ENV === "development",
-  context: { req },
 }));
 
 export default async function graphqlHandler(
@@ -20,5 +19,6 @@ export default async function graphqlHandler(
   res: NextApiResponse
 ) {
   await connect();
-  return handler(req as unknown as Request, res as unknown as Response);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return handler(req as any, res as any);
 }
